17b93075332ea7ed00d930930971a9b6f19cd177
konomoha <konomoha@hotmail.fr>
2022-04-24 02:30:04 +0200

    update Datapersister

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ArticleRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -9,53 +11,63 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 #[ApiResource(
-    normalizationContext: ['groups' => ['read']],
-    denormalizationContext: ['groups' => ['write']],
+    normalizationContext:["groups"=>["article:read"]],
+    denormalizationContext:["groups"=>["article:write"]]
 )]
 class Article
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
-    #[Groups(["read", "write"])]
+    #[Groups(["article:read", "article:write"])]
     private $title;
 
     #[ORM\Column(type: 'string', length: 255)]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $slug;
 
     #[ORM\Column(type: 'text', nullable: true)]
-    #[Groups(["read", "write"])]
+    #[Groups(["article:read", "article:write"])]
     private $content;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
-    #[Groups(["read", "write"])]
+    #[Groups(["article:read", "article:write"])]
     private $picture;
 
     #[ORM\Column(type: 'boolean')]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $isPublished;
 
     #[ORM\Column(type: 'datetime', nullable: true)]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $publishedAt;
 
     #[ORM\Column(type: 'datetime', nullable: true)]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $updatedAt;
 
     #[ORM\Column(type: 'datetime')]
-    #[Groups("read")]
+    #[Groups("article:read")]
     private $createdAt;
+
+    #[ORM\OneToMany(mappedBy: 'article', targetEntity: Comment::class, orphanRemoval: true)]
+    #[Groups("article:read")]
+    private $comments;
+
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'articles', cascade:["persist"])]
+    #[Groups(["article:read", "article:write"])]
+    private $tags;
 
     public function __construct()
     {
         $this->isPublished = false;
         $this->createdAt = new \DateTime();
+        $this->comments = new ArrayCollection();
+        $this->tags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -155,6 +167,60 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        $this->tags->removeElement($tag);
 
         return $this;
     }
